209932b30584318b3ea8b18adbec189bba79ef7e
Andy Pollari <pollari@gmx.ch>
2016-12-17 12:13:51 +0100

    set EventHandler for unmarshaller

--- src/main/java/org/i2g/service/writers/FileWriter.java
+++ src/main/java/org/i2g/service/writers/FileWriter.java
@@ -6,7 +6,10 @@ import org.i2g.model.I2GContainer;
 import java.util.List;
 
 public interface FileWriter {
-    public static final String GITHUB_URL = "https://github.com/polandy/images2gpx";
+    String GITHUB_URL = "https://github.com/polandy/images2gpx";
+    String GPX_SCHEMA_VERSION = "1.1";
+    String PROJECT_NAME = "Images2Gpx";
+
 
     void write(List<I2GContainer> fileLocationMapping, String outputFilePath);
 }
--- src/main/java/org/i2g/service/writers/GpxFileWriterService.java
+++ src/main/java/org/i2g/service/writers/GpxFileWriterService.java
@@ -3,27 +3,45 @@ package org.i2g.service.writers;
 
 import org.i2g.jaxb.types.*;
 import org.i2g.model.I2GContainer;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import javax.annotation.PostConstruct;
+import javax.xml.XMLConstants;
+import javax.xml.bind.*;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service("gpxFileWriter")
-public class GpxFileWriterService implements FileWriter {
+public class GpxFileWriterService implements FileWriter, ResourceLoaderAware {
+
+    private JAXBContext jaxbContext;
+
+    private ResourceLoader resourceLoader;
+
+    @PostConstruct
+    public void init() {
+        try {
+            jaxbContext = JAXBContext.newInstance(GpxType.class);
+        } catch (JAXBException e) {
+            System.out.println("ERROR: could nod initialize the JAXB Context. Terminate program.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+    }
 
     @Override
     public void write(List<I2GContainer> fileLocationMapping, String outputFilePath) {
         try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(GpxType.class);
-
             JAXBElement<GpxType> gpxElement = new ObjectFactory().createGpx(getGpxType(fileLocationMapping));
-
 
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
@@ -31,11 +49,27 @@ public class GpxFileWriterService implements FileWriter {
 
             File file = new File(outputFilePath);
 
-//            jaxbMarshaller.marshal(gpxType, file);
-            jaxbMarshaller.marshal(gpxElement, System.out);
+            jaxbMarshaller.marshal(gpxElement, file);
+
+            validateXml(file);
+
         } catch (JAXBException e) {
             e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private void validateXml(File xmlFile) throws JAXBException, IOException, SAXException {
+        SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Unmarshaller unmarshaller = this.jaxbContext.createUnmarshaller();
+        File xsdFile = resourceLoader.getResource("classpath:/xsd/gpx.xsd").getFile();
+        Schema gpxSchema =  sf.newSchema(xsdFile);
+        unmarshaller.setSchema(gpxSchema);
+        unmarshaller.setEventHandler(new GpxValidationEventHandler());
+        unmarshaller.unmarshal(xmlFile);
     }
 
     private GpxType getGpxType(List<I2GContainer> fileLocationMapping) {
@@ -81,5 +115,10 @@ public class GpxFileWriterService implements FileWriter {
         waypoint.setLat(BigDecimal.valueOf(latitude));
         waypoint.setLon(BigDecimal.valueOf(longitude));
         return waypoint;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
     }
 }
