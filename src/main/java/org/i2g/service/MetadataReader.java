fe049afba9e920d3ed369343720af80ca7fec144
Andy Pollari <pollari@gmx.ch>
2016-12-18 11:30:31 +0100

    implemented getCaptureDate() in MetadataReaderService

--- src/main/java/org/i2g/service/MetadataReader.java
+++ src/main/java/org/i2g/service/MetadataReader.java
@@ -5,6 +5,7 @@ import com.drew.metadata.Metadata;
 import org.i2g.model.I2GContainer;
 
 import java.io.File;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MetadataReader {
@@ -12,4 +13,6 @@ public interface MetadataReader {
     List<I2GContainer> getI2GContainers(List<File> imageFiles);
 
     GeoLocation getGeolocation(Metadata metadata);
+
+    LocalDateTime getCaptureDate(Metadata metadata);
 }
