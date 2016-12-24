2362c4f9d001ae11bd6239bb800229588b912aa8
Andy Pollari <pollari@gmx.ch>
2016-12-24 12:50:31 +0100

    added test for image sort

--- src/test/java/org/i2g/service/MetadataReaderServiceTest.java
+++ src/test/java/org/i2g/service/MetadataReaderServiceTest.java
@@ -5,18 +5,24 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.lang.GeoLocation;
 import com.drew.metadata.Metadata;
 import org.hamcrest.CoreMatchers;
+import org.i2g.model.I2GContainer;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 
 public class MetadataReaderServiceTest {
 
     MetadataReader service = new MetadataReaderService();
+
+    FileReader fileReaderService = new FileReaderService();
     private static final Double TOLERANCE = 0.01;
 
     private static final String IMAGE_WITH_GPS_DATA_PATH = "/images/image_with_gps_data.jpg";
@@ -87,6 +93,43 @@ public class MetadataReaderServiceTest {
         LocalDateTime captureDate = service.getCaptureDate(metadata);
         // then
         Assert.assertThat(captureDate, is(CoreMatchers.nullValue()));
+    }
+
+    @Test
+    public void shouldOrderImagesByCaptureDate() {
+        // given
+        int filesInDirectory = 5;
+        File inputDirectory = new File(this.getClass().getResource("/test-ordered-by-date").getFile());
+        List<File> files = fileReaderService.readFiles(inputDirectory, false);
+        List<I2GContainer> i2GContainers = service.getI2GContainers(files);
+        List<I2GContainer> sortedContainers = new ArrayList<>();
+        sortedContainers.addAll(i2GContainers);
+
+        // when
+        Collections.sort(sortedContainers);
+
+        // then
+        Assert.assertThat(i2GContainers.size(), is(filesInDirectory));
+        Assert.assertThat(i2GContainers, is(sortedContainers));
+    }
+
+    @Test
+    public void shouldOrderImagesByCaptureDateRecursive() {
+        // given
+        int filesInDirectory = 5;
+        int filesInSubDir = 2;
+        File inputDirectory = new File(this.getClass().getResource("/test-ordered-by-date").getFile());
+        List<File> files = fileReaderService.readFiles(inputDirectory, true);
+        List<I2GContainer> i2GContainers = service.getI2GContainers(files);
+        List<I2GContainer> sortedContainers = new ArrayList<>();
+        sortedContainers.addAll(i2GContainers);
+
+        // when
+        Collections.sort(sortedContainers);
+
+        // then
+        Assert.assertThat(i2GContainers.size(), is(filesInDirectory + filesInSubDir));
+        Assert.assertThat(i2GContainers, is(sortedContainers));
     }
 
     private Metadata readMetadata(String filepath) throws ImageProcessingException, IOException {
